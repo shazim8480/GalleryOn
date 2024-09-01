368a7e69b32ee5b934e63e19e7db9c11dad047ae
shazim8480 <troublemaker8480@gmail.com>
2024-09-01 09:57:42 +0600

    global styles added for more reusability

---
+++ src/styles/global.styles.ts
@@ -0,0 +1,13 @@
+import {StyleSheet} from 'react-native';
+
+export const globalStyles = StyleSheet.create({
+  snackbar: {
+    width: '100%',
+    flex: 1,
+    justifyContent: 'center',
+    marginLeft: '5%',
+  },
+  imgCover: {
+    borderRadius: 4,
+  },
+});
